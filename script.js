45c00992d703e01aaaa3caaaffe04287096fab5f
iori-28 <skyzyrym17@gmail.com>
2025-10-04 20:13:32 +0700

    fnl

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const container = document.querySelector(".container");
 const content = document.getElementById("content");
-const overlay = document.createElement("div");
-overlay.className = "overlay";
-document.body.appendChild(overlay);
 
 // Flag untuk cek apakah Home bubble sudah ditambahkan
 let homeVisible = false;
@@ -11,8 +8,10 @@ content.innerHTML = `
     <div class="card welcome">
       <h2>Rio Rasyha Syadzily<br>24416255201209</h2>
       <p>Ini adalah web latihan/tugas dari matkul Pemrogaman Web. Klik salah satu bubble lain untuk melihat isi didalam nya.</p>
+      <img src="Poto_tugas.jpg" alt="Poto Tugas" class="welcome-img">
     </div>
 `;
+
 
 document.querySelectorAll(".bubble").forEach(bubble => {
   bubble.addEventListener("click", () => {
@@ -25,8 +24,9 @@ document.querySelectorAll(".bubble").forEach(bubble => {
       // Kalau klik Home -> hapus bubble Home lagi
       content.innerHTML = `
         <div class="card welcome">
-          <h2>Selamat Datang!</h2>
-          <p>Ini adalah tampilan awal personal web saya. Klik salah satu bubble lain untuk mulai eksplorasi.</p>
+          <h2>Rio Rasyha Syadzily<br>24416255201209</h2>
+          <p>Ini adalah web latihan/tugas dari matkul Pemrogaman Web. Klik salah satu bubble lain untuk melihat isi didalam nya.</p>
+          <img src="Poto_tugas.jpg" alt="Poto Tugas" class="welcome-img">
         </div>
       `;
 
@@ -42,11 +42,35 @@ document.querySelectorAll(".bubble").forEach(bubble => {
         <h2>Galeri</h2>
         <p>Koleksi foto & dokumentasi saya:</p>
         <div class="gallery">
-          <img src="" alt="gambar 1">
-          <img src="" alt="gambar 2">
-          <img src="" alt="gambar 3">
-          <img src="" alt="gambar 4">
-          <img src="" alt="gambar 5">
+          <div class="gallery-item">
+            <img src="https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p2/78/2025/04/20/Monumen-IKN-Jadi-Sorotan-Lorem-Ipsum-Nongol-di-Tugu-Titik-Nol-Kini-Ditutup-3237356032.jpg" alt="gambar artikel 1">
+            <p class="caption">Totem Lorem Ipsum</p>
+          </div>
+
+          <div class="gallery-item">
+            <img src="code_html.png" alt="gambar kodingan html">
+            <p class="caption">Kodingan HTML</p>
+          </div>
+
+          <div class="gallery-item">
+            <img src="code_js.png" alt="gambar kodingan js">
+            <p class="caption">Kodingan JavaScript</p>
+          </div>
+          
+          <div class="gallery-item">
+            <img src="code_css.png" alt="gambar kodingan css">
+            <p class="caption">Kodingan CSS</p>
+          </div>
+          
+          <div class="gallery-item">
+            <img src="Screenshot_hoki.png" alt="gambar hoki">
+            <p class="caption">Hoki Gacha</p>
+          </div>
+          
+          <div class="gallery-item">
+            <img src="Screenshot_hoki2.png" alt="gambar hoki2">
+            <p class="caption">Hoki Gacha 2</p>
+          </div>
         </div>
       `;
       addHomeBubble();
@@ -54,7 +78,7 @@ document.querySelectorAll(".bubble").forEach(bubble => {
 
     else if (bubble.id === "bubble3") {
       content.innerHTML = `
-        <h2>Kontak</h2>
+        <h2>Beberapa akun medsos saya</h2>
         <div class="contact-bubbles">
           <a href="https://github.com/iori-28" target="_blank" class="contact-bubble">
             <i class="fab fa-github"></i>
@@ -65,8 +89,8 @@ document.querySelectorAll(".bubble").forEach(bubble => {
           <a href="https://www.linkedin.com/in/rio-rasyha-syadzily" target="_blank" class="contact-bubble">
             <i class="fab fa-linkedin"></i>
           </a>
-          <a href="#" class="contact-bubble email">
-            <i class="fas fa-envelope"></i>
+          <a href="https://www.youtube.com/@RioRasyhaS_28" target="_blank" class="contact-bubble">
+            <i class="fab fa-youtube"></i>
           </a>
         </div>
       `;
@@ -76,16 +100,17 @@ document.querySelectorAll(".bubble").forEach(bubble => {
     else if (bubble.id === "bubble4") {
       content.innerHTML = `
       <div class="artikel">
-        <h2>Artikel</h2>
+        <h2>Totem di IKN</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique tellus quis eros tincidunt, eget hendrerit metus semper. Nunc malesuada malesuada ex at consequat. Nulla eget lorem sodales, pharetra est sit amet, pretium ante. Nam eget felis id tortor scelerisque pulvinar sed nec libero. Nam at mollis velit. Phasellus tincidunt ipsum sit amet justo accumsan ullamcorper. Duis et molestie enim. Sed rhoncus nisi et neque interdum cursus id at augue.</p>
         <p>Quisque ut dolor aliquam, iaculis erat at, mattis dui. Sed vitae condimentum lectus. Curabitur semper odio at lectus ultricies, ut tincidunt ante fringilla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent non elementum odio. Quisque ornare semper malesuada. Donec porta posuere sagittis. Suspendisse vitae tempus ipsum. Vivamus ex arcu, facilisis vehicula metus id, semper eleifend lorem. Etiam tincidunt aliquam nisi, at congue erat egestas ut. Aenean sed porta magna, ut pharetra mauris.</p>
         <p>Integer justo leo, faucibus sed dignissim eget, molestie at est.Nulla facilisi. Pellentesque ullamcorper nunc in ligula volutpat condimentum. Sed commodo justo justo, vel lacinia ante molestie ac. Vivamus faucibus metus nec sagittis elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam aliquam orci et est volutpat sodales. Proin pharetra efficitur odio, a lacinia sem dapibus a. Etiam nisi mi, facilisis id ex cursus, luctus efficitur ligula. Nulla facilisi. Aenean hendrerit mauris in tortor suscipit mollis. Aenean in orci quis urna convallis laoreet sodales vulputate ligula.</p>
       </div>
-        <p><strong>Contoh tabel:</strong></p>
+        <p><strong>Kurun waktu totem sebelum ditutup:</strong></p>
         <table>
-          <tr><th>Judul</th><th>Tahun</th></tr>
-          <tr><td>Artikel A</td><td>2023</td></tr>
-          <tr><td>Artikel B</td><td>2024</td></tr>
+          <tr><th>Waktu</th><th>Keterangan</th></tr>
+          <tr><td>Januari 2025</td><td>video di TikTok oleh akun @hendrigunawan776883373.</td></tr>
+          <tr><td>Kamis, 17 April 2025</td><td>akun X @jeJAKAki.</td></tr>
+          <tr><td>Sabtu, 19 April 2025</td><td>Otorita IKN secara resmi menutup tugu tersebut dengan terpal biru</td></tr>
         </table>
       `;
       addHomeBubble();
@@ -111,8 +136,11 @@ function addHomeBubble() {
       homeBubble.classList.add("active");
 
       content.innerHTML = `
-        <h2>Rio Rasyha Syadzily<br>24416255201209</h2>
-        <p>Ini adalah web latihan/tugas dari matkul Pemrogaman Web. Klik salah satu bubble lain untuk melihat isi didalam nya.</p>
+        <div class="card welcome">
+          <h2>Rio Rasyha Syadzily<br>24416255201209</h2>
+          <p>Ini adalah web latihan/tugas dari matkul Pemrogaman Web. Klik salah satu bubble lain untuk melihat isi didalam nya.</p>
+          <img src="Poto_tugas.jpg" alt="Poto Tugas" class="welcome-img">
+        </div>
       `;
 
       // Hapus bubble Home saat balik ke Home
@@ -125,28 +153,16 @@ function addHomeBubble() {
   }
 }
 
-const popup = document.createElement("div");
-popup.className = "popup";
-popup.innerHTML = `
-  <h3>Email Saya</h3><br>
-  <p>riorasyhasyadzily@gmail.com</p><br>
-  <button id="closePopup">Tutup</button>
-`;
-document.body.appendChild(popup);
-
-// Event klik di bubble email → munculin popup
-document.addEventListener("click", (e) => {
-  if (e.target.closest(".contact-bubble.email")) {
-    e.preventDefault(); // supaya ga langsung buka mail app
-    popup.style.display = "block";
-    overlay.style.display = "block";
-  }
+// Klik bubble → langsung jadi sidebar
+$(".bubble").on("click", function () {
+  $(".container").addClass("fixed");
 });
 
-// Event klik close → tutup popup
-document.addEventListener("click", (e) => {
-  if (e.target.id === "closePopup" || e.target.className === "overlay") {
-    popup.style.display = "none";
-    overlay.style.display = "none";
+// Scroll → atur posisi bubble
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 150) {
+    $(".container").addClass("fixed");
+  } else {
+    $(".container").removeClass("fixed");
   }
 });
